b2daa63d3898c90fe51ca93898208ac9ead45e96
Ahmed Abdul Rahman <115194258+ahmedabdulrahman108@users.noreply.github.com>
2022-11-17 23:46:57 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new Typed('#typed', {
+    strings: ['UI / UX Design', 'Web Development', 'Mobile App Development'],
+    stringsElement: null,
+    typeSpeed: 50,
+    startDelay: 1200,
+    backSpeed: 20,
+    backDelay: 500,
+    loop: true,
+    loopCount: Infinity,
+});
